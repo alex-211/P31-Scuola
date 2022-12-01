70b8c1032f3bd6b86a809137ffbc09433d7ae49a
Alessandro Porpiglia <114176944+alex-211@users.noreply.github.com>
2022-12-01 17:47:29 +0100

    finished

--- P31-Porpiglia3AINF/P31-Porpiglia3AINF/Form1.cs
+++ P31-Porpiglia3AINF/P31-Porpiglia3AINF/Form1.cs
@@ -22,7 +22,27 @@ namespace P31_Porpiglia3AINF
 
         private void BTcalcola_Click(object sender, EventArgs e)
         {
-              
+            float peso = Convert.ToSingle(NUDpes.Value), usd = 0, eur;
+
+            if (peso < 20)
+            {
+                usd = (peso * 5) + 5;
+            }
+
+            if (peso >= 20 && peso < 50)
+            {
+                usd = peso * 4;
+            }
+            else if (peso > 50)
+            {
+                usd = peso * 3;
+            }
+
+            eur = usd * 1.05f;
+
+            NUDusd.Value = Convert.ToDecimal(usd);
+            NUDeur.Value = Convert.ToDecimal(eur);
+
         }
 
         private void NUDusd_Click(object sender, EventArgs e)
